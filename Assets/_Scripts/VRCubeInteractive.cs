a449a491bffab0d37eedb59bfc32363a72eebce5
Warrick Corfe-Tan <wcor690@UoA.auckland.ac.nz>
2017-12-01 14:34:23 +1300

    First commit, sliders and menus working

---
+++ Assets/_Scripts/VRCubeInteractive.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRCubeInteractive : MonoBehaviour {
+
+    public Renderer cube_renderer;
+    public float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, speed * Time.deltaTime, 0);
+    }
+
+    public void PointerClick()
+    {
+        cube_renderer.material.color = Color.cyan;
+    }
+
+    public void UpdateSpeed(float newSpeed)
+    {
+        speed = newSpeed;
+    }
+
+    public void UpdateScale(float scaleSpeed)
+    {
+        transform.localScale += new Vector3(scaleSpeed * Time.deltaTime, scaleSpeed * Time.deltaTime, scaleSpeed * Time.deltaTime);
+    }
+}
