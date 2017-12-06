06d84d38489bb1dbe7b91df330284b708fda65a6
Ryan Huynh <dhuy228@UoA.auckland.ac.nz>
2017-12-06 13:54:22 +1300

    Added selection outline and slider reset value

--- Assets/ManipulateController.cs
+++ Assets/ManipulateController.cs
@@ -1,17 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ManipulateController : MonoBehaviour {
 
-	GameObject currentGameObj;
+    GameObject currentGameObj;
+    GameObject lastGameObj;
+    public Shader Outline;
+    public Shader Standard;
+    public Slider rotateSlider;
 
-	public void setInteractiveItem(GameObject gameObj){
+    private void Start()
+    {   
+    }
+
+    public void setInteractiveItem(GameObject gameObj){
 		currentGameObj = gameObj;
 		Debug.Log ("Now interacting with " + gameObj);
-	}
+        
+        currentGameObj.GetComponent<Renderer>().material.shader = Outline;
 
-	public void ScaleUp (int newScale)
+        //check whether if the current model is the same as the last model
+        //cube.GetComponent<Renderer>().material.shader = Standard;
+        if (currentGameObj != lastGameObj)
+        {
+            //Deactivate the last object if it's not the same
+            DeactivateLastObject();
+            lastGameObj = currentGameObj;
+        }
+        else
+        {
+            //Deactivate the current object if it's the same
+            currentGameObj.GetComponent<Renderer>().material.shader = Standard;
+            lastGameObj = null;
+            currentGameObj = null;
+        }
+
+        VRInteractable currentObjectInteractable = gameObj.GetComponent<VRInteractable>();
+        rotateSlider.value = currentObjectInteractable.rotateSpeed;
+    }
+
+    private void DeactivateLastObject()
+    {
+        //if there was no last game object
+        if (lastGameObj == null)
+        {
+            return;
+        }
+        //if there was a last game object
+        lastGameObj.GetComponent<Renderer>().material.shader = Standard;
+        lastGameObj = null;
+    }
+
+    public void ScaleUp (int newScale)
 	{
 		float newScaleValue = currentGameObj.transform.localScale.x * newScale;
 		currentGameObj.transform.localScale = new Vector3(newScaleValue, newScaleValue, newScaleValue);
@@ -24,7 +66,7 @@ public class ManipulateController : MonoBehaviour {
 	}
 
 	public void UpdateSpeed(float newSpeed){
-		VRInteractable currentObjectInteractable = currentGameObj.GetComponent<VRInteractable> ();
-		currentObjectInteractable.rotateSpeed = newSpeed;
-	}
+        VRInteractable currentObjectInteractable = currentGameObj.GetComponent<VRInteractable>();
+        currentObjectInteractable.rotateSpeed = newSpeed;
+    }
 }
