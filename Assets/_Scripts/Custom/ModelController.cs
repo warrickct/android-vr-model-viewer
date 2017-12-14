833f570928b266d304b358dcb6a47a62073cc13c
Warrick Corfe-Tan <wcor690@UoA.auckland.ac.nz>
2017-12-14 13:09:17 +1300

    Replaced GVR SDK usage to OVR SDK

---
+++ Assets/_Scripts/Custom/ModelController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelController : MonoBehaviour {
+
+	List<string> modelList = new List<string>();
+
+    public void LoadModel(string modelName)
+    {
+        GameObject modelGameObject = Resources.Load("Models/obj/" + modelName + "/" + modelName) as GameObject;
+        GameObject instancedModel = Instantiate(modelGameObject);
+        //TODO: Make it spawn in a uniform position
+        instancedModel.transform.localPosition = Vector3.zero;
+        instancedModel.transform.localRotation = Quaternion.identity;
+        instancedModel.AddComponent<VRInteractable>();
+    }
+
+    public void ClearAllModels() {
+		GameObject[] modelsList = GameObject.FindGameObjectsWithTag ("Model");
+		foreach (GameObject model in modelsList) {
+			Destroy (model);
+		}
+	}
+
+	public void AddToBatch(string modelName){
+		modelList.Add (modelName);
+	}
+
+	public void InstantiateBatch(){
+		foreach (string modelName in modelList) {
+			LoadModel (modelName);
+		}
+	}
+}
---
+++ Assets/_Scripts/Custom/SkyboxController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxController : MonoBehaviour {
+
+	//TODO: Can make skybox list be updated on start up or when new skybox added.
+	[SerializeField] private Material[] skyboxList;
+
+	//not necessary. Load a default skybox
+	public void Awake(){
+		RenderSettings.skybox = skyboxList [3];
+	}
+
+	public void ChangeSkybox(float sliderValue){
+		int index = (int)sliderValue;
+		RenderSettings.skybox = skyboxList [index];
+	}
+}
